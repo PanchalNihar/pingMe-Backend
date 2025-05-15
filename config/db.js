1c6a7a64280cd21fa2a483a53f498614d8a86562
PanchalNihar <nihar2624@gmail.com>
2025-05-15 17:23:29 +0530

    change in connection

--- config/db.js
+++ config/db.js
@@ -1,12 +1,14 @@
 const mongoose = require("mongoose");
-require("dotenv").config();
+
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI).then(() => {
-      console.log("Connected to MongoDB");
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
     });
-  } catch (error) {
-    console.log(error.message);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (err) {
+    console.error("MongoDB connection failed:", err.message);
     process.exit(1);
   }
 };
